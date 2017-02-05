d12a420ffd47213270d8c3dc6e9430a0f64f5dd2
polachz <20874904+polachz@users.noreply.github.com>
2017-02-05 12:31:36 +0100

    Functional version v0.1

--- set_ssh_key_access.sh
+++ set_ssh_key_access.sh
@@ -1,15 +1,67 @@
 #!/bin/sh
 
-SSH_KEY='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGls9i+zbftGBW7KcNQyVYHmnwhkPT0H57vRjs6ewJhS 57:77:f3:ca:b8:8d:5a:3c:ad:c4:a7:48:0d:aa:08:e5 just-upload-key'
+#title           :set_ssh_key_access.sh
+#description     :created ssh authorized_keys file  
+#author          :zdenek polach ( https://github.com/polachz )
+#date            :05.02.2017
+#version         :0.1
+#usage           :invoke it remotely or locally
+#notes           :You have to create ssh_pub_key file with the key
+#                :or you have to define the SSH_KEY variable here
+#==============================================================================
+#
+# MIT License
+#
+# Copyright (c) 2017 Zdenek Polach
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#==============================================================================
+
+# Uncomment the line with SSH_KEY vvariable and set to the variable the public  
+# part of your SSH key if you don't want to use the ssh_pub_key file.  
+# !!But remember that oo use the ssh_pub_key file is preferred way !!
+# This variable is used only if the ssh_pub_key file doesn't exist
+
+#SSH_KEY='<set a key string here>'
 
 MY_USER=$(whoami)
 
 SSH_KEYFILE_DIR="/home/$MY_USER/.ssh"
 SSH_KEYFILE_PATH="$SSH_KEYFILE_DIR/authorized_keys"
+SSH_KEY_SOURCE_FILE="/home/$MY_USER/ssh_pub_key"
 
-echo "$SSH_KEYFILE_DIR"
-echo "$SSH_KEYFILE_PATH"
+if [ -f "$SSH_KEY_SOURCE_FILE" ]; then
+   SSH_KEY_FINAL=$(grep -o '^[^#]*' "$SSH_KEY_SOURCE_FILE" 2>&1)
+else
+   if [ ! -z "$SSH_KEY" ]; then
+      SSH_KEY_FINAL=$SSH_KEY
+   fi
+fi
 
+if [  -z "$SSH_KEY_FINAL" ]; then
+      echo
+      echo "!!!FATAL ERROR !!!"
+      echo "No Key is specified, cannot continue!!"
+      echo "Please create ssh_pub_key file or set the SSH_KEY variable"
+      echo
+      exit 1
+fi
 # check if we have .ssh dir
 if [ ! -d "$SSH_KEYFILE_DIR" ]; then
    mkdir "$SSH_KEYFILE_DIR"
@@ -17,14 +69,14 @@ fi
 #check if file exist
 if [ ! -f "$SSH_KEYFILE_PATH" ]; then
    #new file - just create and copy the key here
-   echo "$SSH_KEY" > "$SSH_KEYFILE_PATH"
+   echo "$SSH_KEY_FINAL" > "$SSH_KEYFILE_PATH"
 else
    #check if the key is not in the file
    if grep  -q "$SSH_KEY" $SSH_KEYFILE_PATH 2> /dev/null; then
       echo 'Key already exists in authorized_keys'
    else
       #append the key to the file
-      echo "$SSH_KEY" >> "$SSH_KEYFILE_PATH"
+      echo "$SSH_KEY_FINAL" >> "$SSH_KEYFILE_PATH"
    fi
 fi
 # and finally restrict rights to the file and dir
--- ssh_key_upload.bat
+++ ssh_key_upload.bat
@@ -1,8 +1,25 @@
 @echo off
 
+REM title           :ssh_key_upload.bat
+REM description     :configures SSH key access on the remote system  
+REM author          :zdenek polach ( https://github.com/polachz )
+REM date            :05.02.2017
+REM version         :0.1
+REM usage           :ssh_key_upload [user[:password]@]<server>"
 
-REM Script to upload SSH key on the server
-REM To allow SSH key based authentication 
+Rem -------------------------------------------------------------------------
+
+REM MIT license - see the set_ssh_key_access.sh file
+
+REM -------------------------------------------------------------------------
+
+REM Script to automate upload of the SSH key on the server
+REM The key must be defined in the ssh_pub_key file
+REM Alternative (but not preferred) way to specify the key
+REM is to set the SSH_KEY variable in the set_ssh_key_access.sh file
+
+REM This script uses WinSCP to upload appropriate files to the remote
+Rem And invoke configuration scripts 
 
 if "%1" == "" goto usage
 
